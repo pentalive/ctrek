2bf6ce1fa1d9544d00c3a4564b044b25b8fc6062
pentalive <pentalive@users.noreply.github.com>
2016-03-29 14:21:52 -0700

    Enums, Distance calculations, uses Math.h now

--- ctrek.c
+++ ctrek.c
@@ -3,47 +3,79 @@
 #include <stdlib.h>
 #include <time.h>
 #include <ctype.h>
+#include <math.h>
 
 #define TRUE 1
 #define FALSE 0
 
-#define EMTY 0
-#define STAR 1
-#define BASE 2
-#define KLIG 3
-#define ENTP 4
-#define KLIB 5
-#define PTRP 6
 
 #define SCAN 0
 
-#define SRS 0
-#define LRS 1
-#define IDA 2
-#define WDA 3
-#define PHA 4
-#define PHO 5
-#define DMG 6
-#define LSS 7
-
-#define GREEN 0
-#define DOCKED 1
-#define RED 2
 
 /*
 **Global Variables
+*/
+
+enum conditions
+  {
+    DOCKED,   /* Docked next to a starbase with no klingons around */
+    RED,      /* Klingons in this quadrant                         */
+    GREEN     /* Not Docked no klingons                            */
+  };
+
+enum systems
+  {
+    NOT, /* No device type 0 place holder */
+    SRS, /* Short Range Sensors    */
+    LRS, /* Long Range Sensors     */
+    IDA, /* Impulse Drive Assembly */
+    WDA, /* Warp Drive Assembly    */
+    PHA, /* Phasers                */
+    PHO, /* Photon Torpedos        */
+    DMG, /* Damage Control         */
+    LSS  /* Life Support Systems   */
+  };
+
+
+enum thingsinspace
+  {
+    EMTY, /* Empty Space                           */
+    STAR, /* a Star                                */
+    BASE, /* One of our bases                      */
+    KLIG, /* Klingon Ship                          */
+    ENTP, /* US - The heros!                       */
+    KLIB, /* A Klingon Base   -not using these yet */
+    PTRP  /* A photon torpedo -not using these yet */
+  };
+
+
+/* 
+** I am using elements 1 to 8 even though element 0 and 9 are available
+** because of this when I check the nighbors around a specific cell I do not 
+** have to worry about wraping or doing special tests to insure I am looking 
+** in a valid (1-8) cell as [0] and [9] will automatically be empty
+**
+** Because of this (as I am displaying the 1-8 elements my devices must be
+** numbered 1 to 8 inclusive as well.
 */
 
 int quadrant[10][10][4];
 int sector[10][10];
 int device[10];
 
-int eqx,eqy,esx,esy;
-int bases, klingons,dayz;
-int done;
-int cond;
-int ptorps;
-int energy;
+int eqx,eqy,esx,esy;        /* Enterprise Location                         */
+int bases, klingons,dayz;   /* counts of bases, klingons, days to complete */
+int done;                   /* game done? (out of days,energy or klingons?)*/
+int cond;                   /* current sector condition (red,green,docked) */
+int ptorps;                 /* current supply of photon torpedos           */
+int energy;                 /* current supply of energy                    */
+
+
+int distance(int x1, int y1, int x2, int y2) {
+  return (int) sqrtf( (float) (((x1-x2)*(x1-x2)) + ((y1-y2)*(y1-y2)))   );
+}
+
+
 
 /*
 ** setup
@@ -65,6 +97,7 @@ void Setup(void) {
   ptorps = 10;
   
   for(i=1; i<=8; i++) device[i]=99;
+
 }
 
 
@@ -133,27 +166,29 @@ void BuildSector(void) {
 
 void ShortNameDevice(int which) {
   switch (which) {
-  case 1: printf("SRS"); break;
-  case 2: printf("LRS"); break;
-  case 3: printf("WDA"); break;
-  case 4: printf("IDA"); break;
-  case 5: printf("PHA"); break;
-  case 6: printf("PHO"); break;
-  case 7: printf("DMG"); break;
-  case 8: printf("LSS"); break;
+  case NOT: printf("NOT"); break;
+  case SRS: printf("SRS"); break;
+  case LRS: printf("LRS"); break;
+  case WDA: printf("WDA"); break;
+  case IDA: printf("IDA"); break;
+  case PHA: printf("PHA"); break;
+  case PHO: printf("PHO"); break;
+  case DMG: printf("DMG"); break;
+  case LSS: printf("LSS"); break;
   }
 }
 
 void LongNameDevice(int which) {
   switch (which) {
-  case 1: printf("Short Range Sensors"); break;
-  case 2: printf("Long Range Sensors"); break;
-  case 3: printf("Warp Drive Assembly"); break;
-  case 4: printf("Impulse Drive Assembly"); break;
-  case 5: printf("Phasors"); break;
-  case 6: printf("Photon Torpedos"); break;
-  case 7: printf("Damage Controll"); break;
-  case 8: printf("Life Support"); break;
+  case NOT: printf("Device zero");break;
+  case SRS: printf("Short Range Sensors"); break;
+  case LRS: printf("Long Range Sensors"); break;
+  case WDA: printf("Warp Drive Assembly"); break;
+  case IDA: printf("Impulse Drive Assembly"); break;
+  case PHA: printf("Phasors"); break;
+  case PHO: printf("Photon Torpedos"); break;
+  case DMG: printf("Damage Controll"); break;
+  case LSS: printf("Life Support"); break;
   }
 }
 
@@ -218,7 +253,8 @@ void DoDisplay(void) {
       }
 
       if (devicefail(SRS)) {
-	
+	printf("@");
+      } else {	
 	switch (sector[x][y]) {
 	case EMTY: printf("."); break;
 	case STAR: printf("*"); break;
@@ -229,11 +265,8 @@ void DoDisplay(void) {
 	case PTRP: printf("~"); break;
 	default: printf("?"); break;
 	}
-      } else {
-	printf("@");
       }
-
-
+      
       if (esx==x && esy==y) {
 	printf("]");
       } else {
@@ -279,12 +312,16 @@ void DoDisplay(void) {
       } else {
 	printf(" ");
       }
-      
     }
     printf("\n");
   }
+  printf(" Days: %d \n",dayz);
 }
 
+/*
+** get a digit out of the input stream tossing all other character types
+** return as an int between 0 and 9 inclusive.
+*/
 int getdigit(void) {
   char c;
   int i;
@@ -299,9 +336,10 @@ int getdigit(void) {
 
 void DoCommand(void) {
   char c,w;
-  int x,y;
-  int tx,ty;
-  int cdone;
+  int x,y;    /* 1st & 2nd parameters for command usually x and y coord */
+  int tx,ty;  /* Torpedo x and y as the torpedo travels to target       */
+  int cdone;  /* True if Torpedo is moving false to exit move torp loop */
+  int dist;   /* The result of a distance calculation                   */
 
   do {
     c = getchar();
@@ -319,6 +357,7 @@ void DoCommand(void) {
     printf("P phasers %pw Attack using phasors                      \n");
     printf("R Repair  sys Fix systems (5 points unless docked)      \n");
     printf("H Help        This list of commands                     \n");
+    printf("\n\n");
     break;
 
     
@@ -385,11 +424,14 @@ void DoCommand(void) {
       if (devicefail(WDA)) {
 	printf("Warp Field Failure \n");
       } else {
+	dist = distance(eqx,eqy,x,y);
+	printf("Distance %d ",dist);
 	printf("Warp tranist complete - record ship arival at %d,%d\n",x,y);
 	eqx = x;
 	eqy = y;
+	dayz = dayz - dist;
 	BuildSector();
-	energy = energy - 7000;
+	energy = energy - (50*dist);
       }
     }
     break;
@@ -428,7 +470,7 @@ int main(int argc, char ** argv) {
   BuildQuadrant();
   BuildSector();
 
-  dayz = (klingons * 5) / bases;
+  dayz = (klingons * 30) / bases;
 
   printf("You have %d days to seek and destroy %d klingons\n",dayz,klingons);
   printf("with support from %d bases\n",bases);
